562ca77909155fc31b661765b8601286242cbb2a
mdshihab75 <mostakimshihab75@gmail.com>
2026-07-28 23:45:01 +0600

    paginate part fix

--- vite-project/src/components/common/Paginate.jsx
+++ vite-project/src/components/common/Paginate.jsx
@@ -16,11 +16,11 @@ const Paginate = ({ items, itemsPerPage }) => {
     </>
   );
 }
+
 const endOffset = itemOffset + itemsPerPage;
   console.log(`Loading items from ${itemOffset} to ${endOffset}`);
   const currentItems = items.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(items.length / itemsPerPage);
-  
   
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % items.length;
